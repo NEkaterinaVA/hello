62c778ed787cf22ba56eaff0c239b7b40893e1fb
NEkaterinaVA <kotenokfine@mail.ru>
2025-01-17 19:51:05 +0300

    Add rand function

--- hello.go
+++ hello.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
     "github.com/NEkaterinaVA/greetings"
 	"log"
+    "math/rand"
 )
 
 func main() {
     log.Default().SetPrefix("greetings: ")
     log.SetFlags(0)
-
-    message, err := greetings.Hello("Mary")
+    
+    message, err := greetings.Hello("Mary", rand.Intn(3))
     if err != nil{
         log.Fatal(err)
     }
